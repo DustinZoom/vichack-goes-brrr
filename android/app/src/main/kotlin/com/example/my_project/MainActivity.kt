6fbece6c2762ab96ac8fba21443d4ecd88468d06
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-11 12:02:47 +0000

    Front-end code

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dvote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/index.dart
@@ -0,0 +1,7 @@
+// Export pages
+export '/sign_in/sign_in_widget.dart' show SignInWidget;
+export '/voting_page/voting_page_widget.dart' show VotingPageWidget;
+export '/create_account_choose_role/create_account_choose_role_widget.dart'
+    show CreateAccountChooseRoleWidget;
+export '/organiser_dashboard/organiser_dashboard_widget.dart'
+    show OrganiserDashboardWidget;
