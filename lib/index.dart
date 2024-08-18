c8d7fefcb42364bfcc90d5d66df34e1b423548f5
DustinZoom <SinglEcho@outlook.com>
2024-08-18 18:22:22 +1000

    Delete lib directory

--- lib/index.dart
+++
@@ -1,7 +0,0 @@
-// Export pages
-export '/sign_in/sign_in_widget.dart' show SignInWidget;
-export '/voting_page/voting_page_widget.dart' show VotingPageWidget;
-export '/create_account_choose_role/create_account_choose_role_widget.dart'
-    show CreateAccountChooseRoleWidget;
-export '/organiser_dashboard/organiser_dashboard_widget.dart'
-    show OrganiserDashboardWidget;
